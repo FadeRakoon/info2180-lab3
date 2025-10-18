5d7b69b391711fddcb2df42af9de41919bda329e
FadeRakoon <110274955+FadeRakoon@users.noreply.github.com>
2025-10-17 23:07:25 -0500

    Completed Exercise 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -7,5 +7,23 @@ document.addEventListener('DOMContentLoaded', ()=>{
         div.classList.add("square");
     });
 
-})
+    let x = 0
+    squares.forEach(div=>{
+        
+        div.addEventListener("click", ()=>{
+            console.log(x);
+            if (!div.textContent){
+                if (x % 2 === 0){
+                    div.classList.add("X");
+                    div.textContent = "X";
+                } else {
+                    div.classList.add("O");
+                    div.textContent = "O";
+                }
+                x++;
+            }
+        });
+    })
+
+});
     
